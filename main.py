cedecaa51a164d23977c0c2b479a468a98cb57fd
Connor Mendenhall <ecmendenhall@gmail.com>
2012-05-01 20:23:45 +0300

    Added simple pagination.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
+from google.appengine.api import memcache
 from models import Project
 from urlparse import urlparse
 from datetime import datetime, timedelta
@@ -17,9 +18,37 @@ class MainHandler(webapp2.RequestHandler):
                 
 class ProjectHandler(webapp2.RequestHandler):
     def get(self):
+        def make_project_dict(project, project_list):
+            project_dict = {}
+            project_dict['link'] = project.link + '/widget/card.html'
+            project_dict['left'] = project.left
+            timeleft =  project.end - datetime.now() + timedelta(0, 14400)
+            project_dict['timeleft'] = timeleft
+            hours = str(timeleft.seconds / 3600)
+            if timeleft.days < 0:
+                timeleft_str = hours
+            else:
+                timeleft_str = str(timeleft.days) + ' days, ' + hours + ' hours'
+            project_dict['time'] = timeleft_str
+            project_list.append(project_dict)
+
+        def get_next_page(cursor):
+            p = Project.all().with_cursor(cursor)
+            projects = p.filter('left < ', budget)
+            page = projects.fetch(8)
+            cursor = p.cursor()
+            return page, cursor
+        
         project_dicts = []
         input_error = False
         budget_toolow = False
+
+        p = self.request.get('p')
+        try:
+            pages = int(p)
+        except ValueError:
+            pages = 0
+        
         
         sort = self.request.get('sort')
         if sort not in ['soon', 'close']:
@@ -34,27 +63,27 @@ class ProjectHandler(webapp2.RequestHandler):
             budget = initial_budget
                    
         p = Project.all()
-        projects = p.filter('left < ', budget)
-        if projects.count() == 0:
+        all_projects = p.filter('left < ', budget)
+        page = all_projects.fetch(8)
+        cursor = p.cursor()
+        if all_projects.count() == 0:
             budget_toolow = True
-            while projects.count() < 4:
+            while all_projects.count() < 4:
                 budget = budget + 5
                 p = Project.all()
                 projects = p.filter('left < ', budget)
-        for project in projects:
-            project_dict = {}
-            project_dict['link'] = project.link + '/widget/card.html'
-            project_dict['left'] = project.left
-            timeleft =  project.end - datetime.now() + timedelta(0, 14400)
-            project_dict['timeleft'] = timeleft
-            hours = str(timeleft.seconds / 3600)
-            if timeleft.days < 0:
-                timeleft_str = hours
-            else:
-                timeleft_str = str(timeleft.days) + ' days, ' + hours + ' hours'
-            project_dict['time'] = timeleft_str
-            project_dicts.append(project_dict)
+        for project in page:
+            make_project_dict(project, project_dicts)
+        
+        if pages:
+            for page in range(0, pages):
+                page, cursor = get_next_page(cursor)
+                for project in page:
+                    make_project_dict(project, project_dicts)
+        
+        
         project_dicts.sort(key=lambda i: i['timeleft'])
+
         if sort == 'close':
             project_dicts.sort(key=itemgetter('left'))
 
@@ -62,12 +91,15 @@ class ProjectHandler(webapp2.RequestHandler):
         template_dict['project_dicts'] = project_dicts
         template_dict['initial_budget'] = initial_budget
         template_dict['budget'] = budget
-        template_dict['projects'] = len(project_dicts)
+        template_dict['projects'] = all_projects.count()
         template_dict['sort'] = sort
         template_dict['budget_toolow'] = budget_toolow
         
         path = os.path.join(os.path.dirname(__file__), 'templates/projects.html')
         self.response.out.write(template.render(path, template_dict))
+
+    
+
         
 app = webapp2.WSGIApplication([('/', MainHandler),
                                ('/project', ProjectHandler)],
