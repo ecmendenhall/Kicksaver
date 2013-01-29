f025b0124c0b13fd83b121f00dcd6eff6dd1a3f1
Connor Mendenhall <ecmendenhall@gmail.com>
2013-01-28 18:51:17 -0700

    Fixed borked URL parsing in project crawler. Improved error handling in main.py.

--- kickparser.py
+++ kickparser.py
@@ -1,82 +1,72 @@
-#!/usr/bin/env python
-
 from __future__ import division
 from google.appengine.ext import db
-from google.appengine.api import memcache
 from models import Project
-from urllib2 import urlopen 
+from urllib2 import urlopen
 from urlparse import urlparse
 from bs4 import BeautifulSoup
 from operator import itemgetter
 from time import strptime, mktime
 from datetime import datetime
-import urllib2
 
 ENDING_SOON = 'http://www.kickstarter.com/discover/ending-soon'
 SMALL_PROJECTS = 'http://www.kickstarter.com/discover/small-projects'
 
-DATE_FORMAT = '%Y-%m-%dT%H:%M:%S'
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 CRAWL_TIME = datetime.now()
 
 
 def get_ending_projects(pages, url, project_list):
     for page in range(1, pages):
-    
+
         projects_url = url + '?page=' + str(page)
         print projects_url
-        
-        request = urllib2.Request(projects_url)
-        request.add_header('User-Agent', 'Kicksaver-bot/1.0 +http://www.kicksaver.net/')
-        opener = urllib2.build_opener()
-        p = opener.open(request).read()
+
+        p = urlopen(projects_url).read()
         ending_soon = BeautifulSoup(p)
-    
+
         projects = ending_soon.select('.project-card')
         parse_projects(projects, project_list)
 
     return sorted(project_list, key=itemgetter('left'))
-    
+
+
 def parse_projects(projects, project_list):
     for project in projects:
         project_dict = {}
-       
+
         progress_str = project.select('.funded strong')[0].get_text()[:-1]
-        project_dict['progress'] = int(progress_str.replace(',','')) * .01
+        project_dict['progress'] = int(progress_str.replace(',', '')) * .01
         pledged_str = project.select('.pledged strong')[0].get_text()[1:]
-        project_dict['pledged'] = int(pledged_str.replace(',', '').replace('.',''))
+        project_dict['pledged'] = int(pledged_str.replace(',', '').replace('.', ''))
         link_str = project.select('a')[0]['href']
         parsed_link = urlparse(link_str)
-        project_dict['link'] = parsed_link[1] + parsed_link[2]
+        project_dict['link'] = parsed_link[0] + '://' + parsed_link[1] + parsed_link[2]
         end_str = project.select('.ksr_page_timer')[0]['data-end_time']
-        end_struct = strptime(end_str[:-1], DATE_FORMAT)
+        end_struct = strptime(end_str, DATE_FORMAT)
         end_datetime = datetime.fromtimestamp(mktime(end_struct))
         project_dict['end'] = end_datetime
-        
+
         try:
             total = int(project_dict['pledged'] / project_dict['progress'])
         except ZeroDivisionError:
             total = int(project_dict['pledged'] / .01)
-        
-        project_dict['total'] = total
+
+        project_dict['total'] = tota
         project_dict['left'] = int(project_dict['total'] - project_dict['pledged'])
 
         if project_dict['progress'] < 1 and project_dict['total'] > 0:
             project_list.append(project_dict)
 
+
 def save_projects(project_list):
-    projects = []
-    unduped_project_list = []
-    for project in project_list:
-        if project not in unduped_project_list:
-            unduped_project_list.append(project)
-    for p in unduped_project_list:
+    for p in project_list:
         project = Project()
-        project.link = 'http://www.kickstarter.com' + p['link']
-        project.left = p['left'] 
+        project.link = p['link']
+        project.left = p['left']
         project.end = p['end']
         project.saved = CRAWL_TIME
-        projects.append(project)
-    db.put(projects)
+        project.put()
+
 
 def remove_old_projects():
     p = Project.all()
@@ -84,16 +74,15 @@ def remove_old_projects():
     if old_project:
         old_date = old_project.saved
         p = Project.all().filter('saved = ', old_date)
-        db.delete(p) 
+        db.delete(p)
 
 
-def run():        
-    project_list = []   
-    get_ending_projects(20, ENDING_SOON, project_list)
-    get_ending_projects(20, SMALL_PROJECTS, project_list)
+def run():
+    project_list = []
+    get_ending_projects(10, ENDING_SOON, project_list)
+    get_ending_projects(10, SMALL_PROJECTS, project_list)
     remove_old_projects()
     save_projects(project_list)
-    memcache.flush_all()
 
 if __name__ == '__main__':
     run()
--- main.py
+++ main.py
@@ -4,32 +4,32 @@ from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 from google.appengine.api import memcache
 from models import Project
-from urlparse import urlparse
 from datetime import datetime, timedelta
 from operator import itemgetter
 import webapp2
 import os
-import kickparser
 import json
-import logging
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
         self.response.out.write(template.render(path, {}))
 
+
 class AboutHandler(webapp2.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'templates/about.html')
         self.response.out.write(template.render(path, {}))
-                
+
+
 class ProjectHandler(webapp2.RequestHandler):
     def get(self):
         def make_project_dict(project, project_list):
             project_dict = {}
             project_dict['link'] = project.link + '/widget/card.html'
             project_dict['left'] = project.left
-            timeleft =  project.end - datetime.now() + timedelta(0, 14400)
+            timeleft = project.end - datetime.now() + timedelta(0, 14400)
             project_dict['timeleft'] = timeleft
             hours = str(timeleft.seconds / 3600)
             if timeleft.days < 0:
@@ -43,7 +43,6 @@ class ProjectHandler(webapp2.RequestHandler):
             key = str(budget)
             all_projects = memcache.get(key)
             if all_projects is None or update:
-                logging.error("DB QUERY")          
                 p = Project.all()
                 all_projects = p.filter('left <= ', budget)
                 all_projects = list(all_projects)
@@ -56,12 +55,12 @@ class ProjectHandler(webapp2.RequestHandler):
             page = projects.fetch(8)
             cursor = p.cursor()
             return page, cursor
-        
+
         project_dicts = []
-        input_error = False
         budget_toolow = False
+        no_results = False
         number_results = 8
-        
+
         p = self.request.get('p')
         try:
             pages = int(p)
@@ -70,8 +69,8 @@ class ProjectHandler(webapp2.RequestHandler):
 
         more = self.request.get('more')
         if more:
-            pages += 1 
-        
+            pages += 1
+
         sort = self.request.get('sort')
         if sort not in ['soon', 'close']:
             sort = 'soon'
@@ -83,36 +82,47 @@ class ProjectHandler(webapp2.RequestHandler):
         except ValueError:
             initial_budget = 0
             budget = initial_budget
-        
+
         page = get_projects(budget)
-                
+
         if len(page) == 0:
             budget_toolow = True
+            tries = 0
             while len(page) < 4:
                 budget = budget + 5
                 page = get_projects(budget)
-        for project in page:
-            make_project_dict(project, project_dicts)
-        
-        if pages:
-            number_results = pages * 8
-        
-        project_dicts.sort(key=lambda i: i['timeleft'])
-
-        if sort == 'close':
-            project_dicts.sort(key=itemgetter('left'))
+                tries += 1
+                if tries > 50:
+                    no_results = True
+                    break
 
         template_dict = {}
-        template_dict['project_dicts'] = project_dicts[:number_results]
+
+        if not no_results:
+            for project in page:
+                make_project_dict(project, project_dicts)
+
+            if pages:
+                number_results = pages * 8
+
+            project_dicts.sort(key=lambda i: i['timeleft'])
+
+            if sort == 'close':
+                project_dicts.sort(key=itemgetter('left'))
+
+            template_dict['project_dicts'] = project_dicts[:number_results]
+
+        template_dict['no_results'] = no_results
         template_dict['initial_budget'] = initial_budget
         template_dict['budget'] = budget
         template_dict['projects'] = len(page)
         template_dict['sort'] = sort
         template_dict['pages'] = pages
         template_dict['budget_toolow'] = budget_toolow
-        
+
         path = os.path.join(os.path.dirname(__file__), 'templates/projects.html')
         self.response.out.write(template.render(path, template_dict))
+
 
 class MoreHandler(webapp2.RequestHandler):
     def get(self):
@@ -121,8 +131,8 @@ class MoreHandler(webapp2.RequestHandler):
             project_dict = {}
             project_dict['link'] = project.link + '/widget/card.html'
             project_dict['left'] = project.left
-            timeleft =  project.end - datetime.now() + timedelta(0, 14400)
-            project_dict['timeleft'] = (timeleft.days*60*60*24) + timeleft.seconds
+            timeleft = project.end - datetime.now() + timedelta(0, 14400)
+            project_dict['timeleft'] = (timeleft.days * 60 * 60 * 24) + timeleft.seconds
             hours = str(timeleft.seconds / 3600)
             if timeleft.days < 0:
                 timeleft_str = hours
@@ -140,7 +150,7 @@ class MoreHandler(webapp2.RequestHandler):
         sort = self.request.get('sort')
         if sort not in ['soon', 'close']:
             sort = 'soon'
-        
+
         p = Project.all()
         projects = p.filter('left <= ', budget)
         page = projects
@@ -149,7 +159,7 @@ class MoreHandler(webapp2.RequestHandler):
 
         for project in page:
             make_project_dict(project, project_dicts)
-        
+
         if sort == 'soon':
             project_dicts.sort(key=lambda i: i['timeleft'])
 
@@ -160,11 +170,11 @@ class MoreHandler(webapp2.RequestHandler):
         response_dict['projects'] = project_dicts
 
         json_response = json.dumps(response_dict)
-        
+
         self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
         self.response.out.write(json_response)
 
-       
+
 app = webapp2.WSGIApplication([('/', MainHandler),
                                ('/project', ProjectHandler),
                                ('/about', AboutHandler),
